f865368dc7477480c0627151d09cefc9e585f94b
wnf <william@fluxrme.com>
2023-10-24 01:51:26 -0500

    completes oauth lesson through step 8.7

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
-
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const User = require('../models/user');
 
 passport.use(new GoogleStrategy(
   // Configuration object
@@ -9,9 +9,38 @@ passport.use(new GoogleStrategy(
     clientSecret: process.env.GOOGLE_SECRET,
     callbackURL: process.env.GOOGLE_CALLBACK
   },
-  // The verify callback function
-  // Let's use async/await!
+  // The verify callback function...
+  // Marking a function as an async function allows us
+  // to consume promises using the await keyword
   async function(accessToken, refreshToken, profile, cb) {
-    // A user has logged in with OAuth...
+    // When using async/await  we use a
+    // try/catch block to handle an error
+    try {
+      // A user has logged in with OAuth...
+      let user = await User.findOne({ googleId: profile.id });
+      // Existing user found, so provide it to passport
+      if (user) return cb(null, user);
+      // We have a new user via OAuth!
+      user = await User.create({
+        name: profile.displayName,
+        googleId: profile.id,
+        email: profile.emails[0].value,
+        avatar: profile.photos[0].value
+      });
+      return cb(null, user);
+    } catch (err) {
+      return cb(err);
+    }
   }
 ));
+
+//After the verify callback function returns the user document, passport calls the passport.serializeUser() method’s callback passing that same user document as an argument. It is the job of that callback function to return the nugget of data that passport is going to add to the session used to track the user.
+passport.serializeUser(function(user, cb) {
+  cb(null, user._id);
+});
+
+//The passport.deserializeUser() method’s callback function is called every time a request comes in from an existing logged in user. The callback needs to return what we want passport to assign to the req.user object.
+passport.deserializeUser(async function(userId, cb) {
+  // It's nice to be able to use await in-line!
+  cb(null, await User.findById(userId));
+});
